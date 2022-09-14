c316ff985b47d5b5e204fd7488496098019b9524
babix22 <113560704+babix22@users.noreply.github.com>
2022-09-14 23:39:58 +0100

    Add files via upload

---
+++ includes/banner.php
@@ -0,0 +1,39 @@
+<section class="banner">
+	<div class="line-banner"></div>
+	<ul class="menu">
+		<li><a href="index.php#inicio">Início</a></li>	
+		<li><a href="index.php#projetos">Projetos</a></li>
+		<li><a href="index.php#experiencias">Sobre mim</a></li>	
+		<li><a href="index.php#contactos">Contactos</a></li>	
+	</ul>
+	<div class="redes">
+		<div class="circle-rede">
+			<div class="circle-animation"></div>
+			<a href="https://www.linkedin.com/in/b%C3%A1rbara-barbosa-b4990822b/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+		</div>
+		<div class="circle-rede">
+			<div class="circle-animation"></div>
+			<a href="mailto:barbarabarbosa22@hotmail.com" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+		</div>
+		<div class="circle-rede">
+			<div class="circle-animation"></div>
+			<a href="https://www.instagram.com/barbara_barbosa_22/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		</div>
+		<div class="circle-rede">
+			<div class="circle-animation"></div>
+			<i class="fa fa-bars" aria-hidden="true"></i>
+			<i class="fa fa-times" aria-hidden="true"></i>
+
+		</div>
+	</div>
+	<div class="menu-tel">
+		<ul>
+			<li><a href="index.php#inicio">Início</a></li>
+			<li><a href="index.php#projetos">Projetos</a></li>
+			<li><a href="index.php#experiencias">Sobre mim</a></li>	
+			<li><a href="index.php#contactos">Contactos</a></li>	
+		</ul>
+	</div>
+</section>
+
+
